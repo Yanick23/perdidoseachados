eb374e6a48679090ce914a37f51b75949dfb151a
Yanick23 <yanick.ribeiro23@gmail.com>
2024-06-01 16:44:14 +0200

    Dev to Prod

--- src/main/java/com/perdidoseachados/perdidoseachados/PerdidoseachadosApplication.java
+++ src/main/java/com/perdidoseachados/perdidoseachados/PerdidoseachadosApplication.java
@@ -2,12 +2,25 @@ package com.perdidoseachados.perdidoseachados;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PerdidoseachadosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PerdidoseachadosApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer configurer() {
+		return new WebMvcConfigurer () {
+			@Override public void addCorsMappings (CorsRegistry registry ) {
+				// TODO Auto-generated method stub
+				registry.addMapping("/**").allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
 	}
 
 }
